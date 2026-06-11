c59d9bddfe194b0a7494b8ebe55857f543685e88
baihaqiramadhani-11 <baihakiramadhani66@gmail.com>
2026-06-11 18:03:55 +0700

    implement postorder traversal

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -119,3 +119,12 @@ public:
             cout << "Tree is empty" << endl;
             return;
         }
+
+        if (ptr != NULL)
+        {
+            postorder(ptr->leftchild);
+            postorder(ptr->rightchild);
+            cout << ptr->info << " ";
+        }
+    }
+};
