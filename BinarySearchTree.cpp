8987ceda301b15d95ecba2cbe69796b368e1fc05
baihaqiramadhani-11 <baihakiramadhani66@gmail.com>
2026-06-11 18:02:16 +0700

    void preorder

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -93,3 +93,11 @@ public:
             inorder(ptr->rightchild);
         }
     }
+
+    void preorder(Node *ptr)
+    {
+        if (ROOT == NULL)
+        {
+            cout <<"Tree is empty" <<  endl;
+            return;
+        }
